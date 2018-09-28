8e3e98092c8cced7fa90cada724103ab708ec068
Katrina Pritchard <pritckat@gmail.com>
2018-09-28 19:00:04 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'products#index'
   post '/' => 'products#add'
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
